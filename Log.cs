12f2f0f3ac40dedac31bbc6a46eda43858ae47d4
rudra <rudrak1996@gmail.com>
2021-03-11 17:59:53 +0400

    Initial Commit

---
+++ Log.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace LoggingLibrary
+{
+    public static class Log
+    {
+        public static void Info(string message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        public static void Warning(string message)
+        {
+            Trace.TraceWarning(message);
+        }
+
+        public static void Error(string message)
+        {
+            Trace.TraceError(message);
+        }
+    }
+}
